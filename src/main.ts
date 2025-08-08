e869a42a5e5ce918b9e143f8b71afcdd5f4c40a4
omgSRT <thangckdt@gmail.com>
2025-08-08 20:38:56 +0700

    fix: vercel config

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,12 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, documentFactory(), {
     jsonDocumentUrl: 'swagger/json',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
   });
 
   const configService = app.get(ConfigService);
